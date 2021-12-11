d32d475e65a89ef7c1128b14a8f31bc8d6857daa
Kevin <sxzz@sxzz.moe>
2021-12-11 22:56:01 +0800

    refactor(utils)!: remove global config

--- packages/hooks/use-common-props/index.ts
+++ packages/hooks/use-common-props/index.ts
@@ -1,5 +1,4 @@
 import { ref, unref, inject, computed } from 'vue'
-import { useGlobalConfig as useGlobalConfigLegacy } from '@element-plus/utils/util'
 import { elFormItemKey, elFormKey } from '@element-plus/tokens'
 import { buildProp, componentSize } from '@element-plus/utils/props'
 import { useProp } from '../use-prop'
@@ -21,9 +20,6 @@ export const useSize = (
 
   const size = ignore.prop ? emptyRef : useProp<ComponentSize>('size')
   const globalConfig = ignore.global ? emptyRef : useGlobalConfig('size')
-  const globalConfigLegacy = ignore.global
-    ? { size: undefined }
-    : useGlobalConfigLegacy()
   const form = ignore.form ? { size: undefined } : inject(elFormKey, undefined)
   const formItem = ignore.formItem
     ? { size: undefined }
@@ -36,7 +32,6 @@ export const useSize = (
       formItem?.size ||
       form?.size ||
       globalConfig.value ||
-      globalConfigLegacy.size ||
       ''
   )
 }
